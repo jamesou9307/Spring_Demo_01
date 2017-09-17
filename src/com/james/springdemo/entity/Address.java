ed2f763e3dc6749565d494a31c7ac4e390417fab
JAMES <291387044@qq.com>
2017-09-18 06:46:15 +0800

    lib commit

---
+++ src/com/james/springdemo/entity/Address.java
@@ -0,0 +1,9 @@
+package com.james.springdemo.entity;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Address {
+
+ApplicationContext context=new ClassPathXmlApplicationContext();
+}
