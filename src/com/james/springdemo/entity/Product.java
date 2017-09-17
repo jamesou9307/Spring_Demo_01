040ed14dc05fc8a85a83ab5a31b3229d6febf039
fsuser <291387044@qq.com>
2017-09-17 22:55:27 +0800

    first commit

---
+++ src/com/james/springdemo/entity/Product.java
@@ -0,0 +1,11 @@
+package com.james.springdemo.entity;
+
+import javafx.application.Application;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+
+public class Product {
+
+private BeanClassLoaderAware beanClassLoaderAware;
+}
+
+
