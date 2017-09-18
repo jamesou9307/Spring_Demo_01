c0a6a5377c1de9117d0c6167aac61708cab87e4d
fsuser <291387044@qq.com>
2017-09-18 08:21:27 +0800

    first commit

--- src/com/james/springdemo/entity/Address.java
+++ src/com/james/springdemo/entity/Address.java
@@ -2,8 +2,10 @@ package com.james.springdemo.entity;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Address {
 
 ApplicationContext context=new ClassPathXmlApplicationContext();
+ApplicationContext applicationContext1= new FileSystemXmlApplicationContext();
 }
